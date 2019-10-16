b06302c18c051f3077372a3cc6d008b64e3d4aa8
info <info@info17.cshl.edu>
2019-10-16 15:54:24 -0400

    added nested loops

--- ProblemSets/Python2/evaluate_number.py
+++ ProblemSets/Python2/evaluate_number.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
 
-count = 0
+count = 50
 
 if count > 0:
-	print(count, 'is positive')
+	#print(count, 'is positive')
+	if count < 50:
+		#print(count, 'is smaller than 50')
+		if count % 2 == 0:
+			print(count, 'is an even number that is smaller than 50')
+	elif count > 50:
+		if count % 3 == 0:
+			print(count, 'is larger than 50 and divisible by 3')
+	else:
+		print(count, 'must be equal to 50')
 elif count < 0:
 	print(count, 'is negative')
 elif count == 0:
