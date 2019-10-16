c7eafc59363558e55096cca2534620c9f678ba72
info <info@info17.cshl.edu>
2019-10-16 10:40:48 -0400

    added favorite things

--- first_python_script.py
+++ first_python_script.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
 
 myname = 'Mareike'
+mycolor = 'Blue'
+myhobby = 'Birding'
+myanimal = 'Cats'
 
 print('My name:',myname)
+print('My favorite color:',mycolor)
+print('My favorite activity:',myhobby)
+print("'My favorite animal:',myanimal)
