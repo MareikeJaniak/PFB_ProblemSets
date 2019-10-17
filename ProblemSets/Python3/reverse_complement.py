cec0a1a04c26826a795ca7475d23afb1b03490ab
info <info@info17.cshl.edu>
2019-10-17 13:17:43 -0400

    added comment to code

--- ProblemSets/Python3/reverse_complement.py
+++ ProblemSets/Python3/reverse_complement.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 
+#string to reverse complement
 dna_tmp = 'GATGGGATTggggttttccccTCCCATGTGCTCAAGACTGGCGCTaaaaGttttGAGCTTCTCaaaaGTCTAGAGCCACCGTCCAGGGAGCAGGTAGCTGCTGGGCTCCggggACACTTTGCGTTCGGGCTGGGAGCGTGCTTTCCACGACGGTGACACGCTTCCCTGGATTGGCAGCCAGACTGCCTTCCGGGTCACTGCCATGGAGGAGCCGCAGTCAGATCCTAGCGTCGAGccccCTCTGAGTCAGGAAACAttttCAGACCTATGGAAACTACTTCCTGaaaaCAACGTTCTGTccccCTTGCCGTCCCAAGCAATGGATGATTTGATGCTGTccccGGACGATATTGAACAATGGTTCACTGAAGACCCAGGTCCAGATGAAGCTCCCAGAATTCGCCAGAGGCTGCTccccCCGTGGccccTGCACCAGCAGCTCCTACACCGGCGGccccTGCACCAGccccCTCCTGGccccTGTCATCTTCTGTCCCTTCCCAGaaaaCCTACCAGGGCAGCTACGGTTTCCGTCTGGGCTTCTTGCATTCTGGGACAGCCAAGTCTGTGACTTGCACGTACTccccTGCCCTCAACAAGATGttttGCCAACTGGCCAAGACCTGCCCTGTGCAGCTGTGGGTTGATTCCACAccccCGCCCGGCACCCGCGTCCGCGCCATGGCCATCTACAAGCAGTCACAGCACATGACGGAGGTTGTGAGGCGCTGccccCACCATGAGCGCT'
 
-dna = dna_tmp.upper()
+dna = dna_tmp.upper()    #make sequence all upper case
 
-dna_complement = dna.replace('A', 't').replace('C','g').replace('T','A').replace('G','C')
+dna_complement = dna.replace('A', 't').replace('C','g').replace('T','A').replace('G','C')			#replace each letter with complement, but make first two lower case to avoid replacing initial replacements
 
-dna_complement_upper = dna_complement.upper()
-dna_reverse_complement = dna_complement_upper[::-1]
+dna_complement_upper = dna_complement.upper()					#return everything to upper case
+dna_reverse_complement = dna_complement_upper[::-1]		#reverse the sequence
 #dna_reverse_complement = "".join(reversed(dna_complement_upper))
 
 string = 'Original Sequence\t5\'{} 3\'\nComplement\t3\'{} 5\'\nReverse Complement\t5\'{} 3\''
