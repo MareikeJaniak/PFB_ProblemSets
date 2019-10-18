168e1181a2c439ce0b1f3717e1913dcb046263af
info <info@info17.cshl.edu>
2019-10-18 10:42:17 -0400

    changed output from STDOUT to file

--- ProblemSets/Python6/modify_text.py
+++ ProblemSets/Python6/modify_text.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
-with open("./Python_06.txt", 'r') as text_read:
+with open("./Python_06.txt", 'r') as text_read, open("./Python_06_uc.txt", 'w') as text_print:
 	for line in text_read:
-		print(line.upper())
+		text_print.write(line.upper() + "\n")
+
+print("Wrote upper case text to file 'Python_06_uc.txt'")
