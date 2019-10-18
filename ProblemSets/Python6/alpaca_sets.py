b0e60f48b2d1f6850b1ffa4970f8a8bc185cedf9
info <info@info17.cshl.edu>
2019-10-18 13:23:03 -0400

    added transcription factor list comparison

--- ProblemSets/Python6/alpaca_sets.py
+++ ProblemSets/Python6/alpaca_sets.py
@@ -3,6 +3,7 @@
 all_genes_set = set()
 stemcell_set = set()
 pigmentation_set = set()
+transcription_set = set()
 
 with open('./alpaca_all_genes.tsv', 'r') as all_genes:
 	for line in all_genes:
@@ -25,5 +26,13 @@ with open('./alpaca_pigmentation_genes.tsv','r') as pig_genes:
 			gene_id,gene_stable,gene_transcript = line.split('\t')
 			pigmentation_set.add(gene_id)
 
+with open('./alpaca_transcriptionFactors.tsv','r') as transcription_factors:
+	for line in transcription_factors:
+		if 'Gene' not in line:
+			line = line.rstrip()
+			gene_id,gene_stable,gene_transcript = line.split('\t')
+			transcription_set.add(gene_id)
+
 #print(all_genes_set - stemcell_set)
-print(stemcell_set & pigmentation_set)
+print('Genes that are both stemcell proliferation genes and pigment genes:',stemcell_set & pigmentation_set)
+print('Genes that are transcription factors for cell proliferation:',transcription_set & stemcell_set)
