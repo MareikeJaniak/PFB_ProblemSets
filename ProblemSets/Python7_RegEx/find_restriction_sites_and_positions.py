786f5640246e3d8221cac29a2acfeb2158d4f63e
info <info@info17.cshl.edu>
2019-10-18 21:36:03 -0400

    added some more code but not solved yet

--- ProblemSets/Python7_RegEx/find_restriction_sites_and_positions.py
+++ ProblemSets/Python7_RegEx/find_restriction_sites_and_positions.py
@@ -4,5 +4,6 @@ import re
 
 with open('./Python_07_ApoI.fasta', 'r') as fasta:
 	fasta = fasta.read()
-	for (pre, post) in re.findall(r"([AG])(AATT[CT])", fasta):
-		print(pre+"^"+post)
+	fasta = re.sub(r"[\n]","", fasta)
+	for (pre, post, middle, pre2, post2) in re.findall(r"([AG])(AATT[CT])([A-Z]+)([AG])(AATT[CT])", fasta):
+		print(pre+"^"+post+pre2+"^"+post2)
