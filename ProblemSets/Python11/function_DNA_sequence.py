be832e4397d364f48ac23e4b7b2404e643292e44
info <info@info17.cshl.edu>
2019-10-21 11:38:45 -0400

    added methods to Class for DNA sequences

--- ProblemSets/Python11/function_DNA_sequence.py
+++ ProblemSets/Python11/function_DNA_sequence.py
@@ -8,10 +8,34 @@ class DNA_Sequence(object):
 		self.name = name
 		self.organism = organism
 
+	def get_length(self):
+		length = len(self.sequence)
+		return length
 
+	def nuc_comp(self):
+		dna_set = set(self.sequence)
+		nt_count = {}
+		for nt in dna_set:
+			count = self.sequence.count(nt)
+			nt_count[nt] = count
+		return nt_count
+
+	def gc_content(self):
+		dna = self.sequence
+		c_count = dna.count('C')
+		g_count = dna.count('G')
+		gc_content = (c_count + g_count) / len(dna)
+		return gc_content
+
+	def fasta_format(self):
+		return('>'+self.name+'\n'+self.sequence)
 
 gene = DNA_Sequence('ATGGACTCATTGGCCAGTC', 'TST1', 'Alouatta palliata')
 
 print(gene.sequence)
 print(gene.name)
 print(gene.organism)
+print('length: {}'.format(gene.get_length()))
+print('nucleotide composition: {}'.format(gene.nuc_comp()))
+print('GC content: {:.2%}'.format(gene.gc_content()))
+print(gene.fasta_format())
